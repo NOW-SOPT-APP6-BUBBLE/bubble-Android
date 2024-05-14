fa21b76a18ceb9a6581dd30c5c51ac2675aaf54f
arinming <jyandysys1@naver.com>
2024-05-14 15:35:12 +0900

    [DELETE/#3] 불필요한 코드 삭제

--- app/src/main/java/com/sopt/bubble/feature/nav/BubbleBottomNavigation.kt
+++ app/src/main/java/com/sopt/bubble/feature/nav/BubbleBottomNavigation.kt
@@ -8,10 +8,8 @@ import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
