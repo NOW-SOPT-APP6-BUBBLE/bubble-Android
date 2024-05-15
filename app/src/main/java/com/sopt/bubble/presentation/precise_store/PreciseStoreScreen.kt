a6e750593203cadeb694f17d09f51c23e3abd16a
boiledegg <tjrwns2163@naver.com>
2024-05-15 15:50:03 +0900

    [UI/#4] TopAppBar 추가

--- app/src/main/java/com/sopt/bubble/presentation/precise_store/PreciseStoreScreen.kt
+++ app/src/main/java/com/sopt/bubble/presentation/precise_store/PreciseStoreScreen.kt
@@ -1,2 +1,76 @@
 package com.sopt.bubble.presentation.precise_store
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.bubble.R
+import com.sopt.bubble.ui.theme.Headline02
+
+@Composable
+fun PreciseStoreScreen() {
+    Scaffold(
+        topBar = { PreciseStoreTopBar() }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PreciseStoreTopBar() {
+    TopAppBar(
+        title = {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp, vertical = 12.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_back),
+                        contentDescription = stringResource(id = R.string.app_bar_content_description_back)
+                    )
+                    Spacer(modifier = Modifier.width(15.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.precise_store_app_bar_header),
+                        style = Headline02,
+                    )
+                }
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = stringResource(id = R.string.app_bar_content_description_close)
+                )
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreciseScreenPreview() {
+    PreciseStoreScreen()
+}
