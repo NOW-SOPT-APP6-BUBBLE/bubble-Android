301f073f957e52a06d2b8c641636b7bbce79d756
LEE YOU BIN <youbeene3409@naver.com>
2024-05-16 23:53:31 +0900

    [FEAT/#7] 화면 추가

---
+++ app/src/main/java/com/sopt/bubble/feature/friends/detail/DetailScreen.kt
@@ -0,0 +1,28 @@
+package com.sopt.bubble.feature.friends.detail
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.bubble.ui.theme.BubbleAndroidTheme
+import com.sopt.bubble.ui.theme.Headline01
+
+@Composable
+fun DetailScreen() {
+    Column (
+
+    ){
+        Text(
+            text = "Friends",
+            style = Headline01
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BubbleAndroidTheme {
+        DetailScreen()
+    }
+}
