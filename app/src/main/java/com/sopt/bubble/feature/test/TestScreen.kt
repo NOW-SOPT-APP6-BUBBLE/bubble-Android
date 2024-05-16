d783a0fe3c6c132725ce43629ef88341d8bd2e04
arinming <jyandysys1@naver.com>
2024-05-16 18:47:27 +0900

    [FEAT/#3] 바텀 네비 없는 Test 스크린 이동

--- app/src/main/java/com/sopt/bubble/feature/test/TestScreen.kt
+++ app/src/main/java/com/sopt/bubble/feature/test/TestScreen.kt
@@ -1,4 +1,4 @@
-package com.sopt.bubble.feature.chat
+package com.sopt.bubble.feature.test
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -6,20 +6,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import com.sopt.bubble.feature.nav.Screen
-import com.sopt.bubble.ui.theme.Headline01
+import com.sopt.bubble.feature.nav.BottomScreen
 
 @Composable
-fun ChatScreen(
+fun TestScreen(
     onNavigate: NavHostController,
 ) {
     Column {
         Text(
-            modifier = Modifier.clickable {
-                onNavigate.navigate(Screen.Test.route)
-            },
-            text = "Chat",
-            style = Headline01
+            modifier = Modifier.clickable { onNavigate.navigate(BottomScreen.Friends.route) },
+            text = "Test"
         )
     }
 }
