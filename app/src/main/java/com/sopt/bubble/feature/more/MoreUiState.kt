5a740bcbf0b9108e1104cd2ff8442ea1ae62a2ce
boiledegg <tjrwns2163@naver.com>
2024-05-18 01:59:03 +0900

    [MOV/#5] 뷰모델과 State 클래스 분리

---
+++ app/src/main/java/com/sopt/bubble/feature/more/MoreUiState.kt
@@ -0,0 +1,6 @@
+package com.sopt.bubble.feature.more
+
+data class MoreUiState(
+    val nickName: String = "언니",
+    val email: String = "612240@naver.com"
+)
--- app/src/main/java/com/sopt/bubble/feature/more/MoreViewModel.kt
+++ app/src/main/java/com/sopt/bubble/feature/more/MoreViewModel.kt
@@ -6,10 +6,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-data class MoreUiState(
-    val nickName: String = "언니",
-    val email: String = "612240@naver.com"
-)
 class MoreViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(MoreUiState())
     val uiState: StateFlow<MoreUiState> = _uiState.asStateFlow()
