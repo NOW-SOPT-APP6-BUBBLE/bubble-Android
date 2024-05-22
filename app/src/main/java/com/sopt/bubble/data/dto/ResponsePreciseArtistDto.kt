f61c1555f94a779b8a6cb2506d76321b482dc28e
boiledegg <tjrwns2163@naver.com>
2024-05-23 01:31:13 +0900

    [ADD/#19] 응답 DTO 생성

---
+++ app/src/main/java/com/sopt/bubble/data/dto/ResponsePreciseArtistDto.kt
@@ -0,0 +1,48 @@
+package com.sopt.bubble.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponsePreciseArtistDto(
+    @SerialName("status")
+    val status: Int,
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("message")
+    val message: String,
+    @SerialName("result")
+    val result: Result,
+){
+    @Serializable
+    data class Result(
+        @SerialName("artist")
+        val artistList: List<Artist>
+    ) {
+        @Serializable
+        data class Artist(
+            @SerialName("name")
+            val name: String,
+            @SerialName("description")
+            val description: String,
+            @SerialName("photo")
+            val photo: String,
+            @SerialName("subscribes")
+            val subscribes: List<Subscribe>,
+            @SerialName("isServiceMember")
+            val isServiceMember: List<String>,
+            @SerialName("isNotServiceMember")
+            val isNotServiceMember: List<String>
+        ) {
+            @Serializable
+            data class Subscribe(
+                @SerialName("name")
+                val name: String,
+                @SerialName("previousPrice")
+                val previousPrice: Int,
+                @SerialName("price")
+                val price: Int
+            )
+        }
+    }
+}
