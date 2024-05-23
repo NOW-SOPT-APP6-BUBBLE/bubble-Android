01484a67699462dfae8f41c47d3f439139c39359
LEE YOU BIN <youbeene3409@naver.com>
2024-05-23 16:18:57 +0900

    [CHORE/#22] 주석

--- app/src/main/java/com/sopt/bubble/feature/friends/detail/FriendDetailViewModel.kt
+++ app/src/main/java/com/sopt/bubble/feature/friends/detail/FriendDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.sopt.bubble.feature.friends.detail
 
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.bubble.module.ServicePool.friendDetailService
@@ -8,6 +9,8 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 
 class FriendDetailViewModel : ViewModel() {
+
+    // sharedFlow 수정 필요
 
     private val _sideEffect: MutableSharedFlow<FriendDetailSideEffect> = MutableSharedFlow()
     val sideEffect: SharedFlow<FriendDetailSideEffect> get() = _sideEffect
