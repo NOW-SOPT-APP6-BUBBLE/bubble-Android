be4a2512c46daa82bb9d276647d22c8d4a67bc7a
LEE YOU BIN <youbeene3409@naver.com>
2024-05-24 01:07:13 +0900

    [CHORE/#22] 코드 수정

--- app/src/main/java/com/sopt/bubble/feature/friends/detail/FriendDetailState.kt
+++ app/src/main/java/com/sopt/bubble/feature/friends/detail/FriendDetailState.kt
@@ -3,6 +3,5 @@ package com.sopt.bubble.feature.friends.detail
 sealed class FriendDetailState {
     data object Empty : FriendDetailState()
     data object Loading : FriendDetailState()
-
     data object Success : FriendDetailState()
 }
