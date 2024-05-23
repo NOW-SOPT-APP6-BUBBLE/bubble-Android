3dc0cf823d956ed4dd95c4f5623d85f8df20e37e
boiledegg <tjrwns2163@naver.com>
2024-05-23 18:53:30 +0900

    [FEAT/#27] AsyncImage 추가

--- app/src/main/java/com/sopt/bubble/feature/precise_store/PreciseStoreScreen.kt
+++ app/src/main/java/com/sopt/bubble/feature/precise_store/PreciseStoreScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.bubble.feature.precise_store
 
+import android.util.Log
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
@@ -26,11 +27,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.sopt.bubble.R
 import com.sopt.bubble.data.dto.response.ResponsePreciseArtistDto
 import com.sopt.bubble.feature.precise_store.PreciseStoreViewModel.Companion.CHECK_BUTTON_NUM
@@ -105,14 +109,16 @@ fun PreciseStoreSuccessScreen(
                 .verticalScroll(scrollState)
                 .background(Gray700)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(uiState.photo)
+                    .crossfade(true)
+                    .build(),
                 contentDescription = stringResource(id = R.string.app_name),
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(PRECISE_STORE_TOP_IMAGE_RATIO)
-            )
+                    .aspectRatio(PRECISE_STORE_TOP_IMAGE_RATIO))
 
             PreciseStoreArtistDescription(
                 uiState = uiState,
